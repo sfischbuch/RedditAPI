fbdcbb0abc53fc1c7f902b3fa32a63d2a8f85d54
jonwilami323 <jonwilami@gmail.com>
2021-07-11 08:07:17 -0400

    The redditpraw script now retrieves more data
    - Added two more data files
    - Made improvements to redditpraw to get more data

--- redditpraw.py
+++ redditpraw.py
@@ -2,17 +2,48 @@ import praw
 import pandas as pd
 from itertools import cycle
 
+#Create Reddit instance and set configuration. 
+# TODO add try except blocks here
 reddit = praw.Reddit('bot1', config_interpolation="basic")
 
-redditors = ["an-average-white-guy"]
+#All possible MBTI Types
+MTBTI_TYPES = ["ISTJ", "ISTP", "ISFJ", "ISFP", "INFJ", "INFP", "INTJ", "INTP", 
+            "ESTP", "ESTJ", "ESFP", "ESFJ", "ENFP", "ENFJ", "ENTP", "ENTJ"]
+COLS = ["redditor", "type", "text"]
+data = []
 
-type = ["INTJ"]
-texts = []
+#Add unique redditors from the a particular subreddit.
+users = set()
+for comment in reddit.subreddit("ENFP").comments(limit=100):
+     a = comment.author
+     users.add(str(a))
 
-for submission in reddit.redditor(redditors[0]).submissions.new(limit=10):
-    texts.append(submission.selftext)
-    #print(submission.selftext)
+for user in users:
+    type = ""
+    texts = []
 
-data = list(zip(cycle(redditors), cycle(type), texts))
-reddit_df = pd.DataFrame(data, columns='redditor, type, text'.split())
-reddit_df.to_csv(r'.\data\personality_data.csv', index = False, header=True)
+    # Find the user's flair type.
+    flairs = []
+    for submission in reddit.redditor(user).submissions.new(limit=35):
+        if submission.author_flair_text in MTBTI_TYPES:
+            flairs.append(submission.author_flair_text)
+    if flairs:
+        type = max(set(flairs), key=flairs.count)
+
+        for submission in reddit.redditor(user).submissions.new(limit=10):
+            if submission.selftext:
+                texts.append(submission.selftext)
+        
+            if texts:
+                data.append(list(zip(cycle([user]), cycle([type]), texts)))
+
+    del texts[:]
+    del flairs[:]
+
+reddit_data = [item for sublist in data for item in sublist]
+
+if reddit_data:
+    reddit_df = pd.DataFrame(reddit_data, columns=COLS)
+    reddit_df.to_csv(r'.\data\personality_data3.csv', index = False, header=True)
+
+# # TODO add main function and interface 
