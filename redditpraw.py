36ada31c5b0edcec1ba4f1aab824ffaf7342d078
jonwilami323 <jonwilami@gmail.com>
2021-07-11 09:34:34 -0400

    Fixed data duplication bug

--- redditpraw.py
+++ redditpraw.py
@@ -12,9 +12,17 @@ MTBTI_TYPES = ["ISTJ", "ISTP", "ISFJ", "ISFP", "INFJ", "INFP", "INTJ", "INTP",
 COLS = ["redditor", "type", "text"]
 data = []
 
-#Add unique redditors from the a particular subreddit.
+# for submission in reddit.redditor("adrianaie").submissions.new(limit=35):
+#     if submission.author_flair_text in MTBTI_TYPES:
+#         print("{} {} {} {} {} {} ".format(submission.author_flair_text,
+#         submission.author,
+#         submission.created_utc,
+#         submission.id,
+#         submission.name,
+#         submission.title))
+# Add unique redditors from the a particular subreddit.
 users = set()
-for comment in reddit.subreddit("ENFP").comments(limit=100):
+for comment in reddit.subreddit("ENFP").comments(limit=None):
      a = comment.author
      users.add(str(a))
 
@@ -24,18 +32,18 @@ for user in users:
 
     # Find the user's flair type.
     flairs = []
-    for submission in reddit.redditor(user).submissions.new(limit=35):
+    for submission in reddit.redditor(user).submissions.new(limit=None):
         if submission.author_flair_text in MTBTI_TYPES:
             flairs.append(submission.author_flair_text)
     if flairs:
         type = max(set(flairs), key=flairs.count)
 
-        for submission in reddit.redditor(user).submissions.new(limit=10):
+        for submission in reddit.redditor(user).submissions.new(limit=None):
             if submission.selftext:
                 texts.append(submission.selftext)
         
-            if texts:
-                data.append(list(zip(cycle([user]), cycle([type]), texts)))
+        if texts:
+            data.append(list(zip(cycle([user]), cycle([type]), texts)))
 
     del texts[:]
     del flairs[:]
@@ -44,6 +52,6 @@ reddit_data = [item for sublist in data for item in sublist]
 
 if reddit_data:
     reddit_df = pd.DataFrame(reddit_data, columns=COLS)
-    reddit_df.to_csv(r'.\data\personality_data3.csv', index = False, header=True)
+    reddit_df.to_csv(r'.\data\personality_data4.csv', index = False, header=True)
 
 # # TODO add main function and interface 
